ba4781eb2be599cd6608e70ea4a52567c16589ce
Remo Grillo <remogrillo@MacBook-Pro-di-Remo.local>
2020-06-22 11:41:35 +0200

    Rome added (capital)

--- pre-processing/geonames/geonames_tsv_to_json.py
+++ pre-processing/geonames/geonames_tsv_to_json.py
@@ -2,7 +2,7 @@ import csv
 import json
 
 jason = {}
-accepted_feature_codes = ["PPLA", "PPLA2", "PPLA3"]
+accepted_feature_codes = ["PPLA", "PPLA2", "PPLA3", "PPLC"]
 
 with open('geonames_it.tsv') as f:
     for row in csv.DictReader(f, delimiter='\t'):
@@ -15,7 +15,7 @@ with open('geonames_it.tsv') as f:
 
         #Get only geonames for:
             #1: Islands or Lagoon in VE (Provincia di venezia)
-            #2: Cities in italy = PPLA (città capoluoghi), PPLA2 (città capoluogo di provincia), PPLA3(città comune)
+            #2: Cities in italy = PPLA(PPLC) (città capoluoghi), PPLA2 (città capoluogo di provincia), PPLA3(città comune)
         if ((feature_code == "ISL" or feature_code == "LGN") and admin2_code == "VE") or feature_code in accepted_feature_codes:
             if not name in jason:
                 jason[name] = []
