680b0c5e7ef19563b71e5e10a5727b6ce91083d9
gspinaci <gianmarco.spinaci@gmail.com>
2020-09-14 22:36:41 +0200

    WIP change people structure

--- pre-processing/builtwork/pre_process_builtwork.py
+++ pre-processing/builtwork/pre_process_builtwork.py
@@ -8,7 +8,7 @@ import json
 
 # TODO pass the file as arg
 dir_path = os.path.dirname(os.path.realpath(__file__))
-filename = os.path.join(dir_path, 'SS_BLDG_01.csv')
+filename = os.path.join(dir_path, 'buildings.csv')
 
 bw_typologies_filename = os.path.join(dir_path, 'dict', 'bw_typologies.tsv')
 bw_uses_filename = os.path.join(dir_path, 'dict', 'bw_uses.tsv')
@@ -150,39 +150,39 @@ df = pd.read_csv(filename, sep='\t')
 
 builtworks = {}
 
+def clear(val):
+  if val is np.nan or val == 'nan': 
+    val = ""
+  return val
+
 # Create a dict with an array containing every bw as json object
 for i in range(len(df)) :
   row_id = df.loc[i, key_bw_id]
-  new_row = {}
 
   if row_id not in builtworks:
-    builtworks[row_id] = []
+    builtworks[row_id] = {}
 
   for y in range(1, len(df.columns)):
-    val = df.iloc[i, y]
+    val = clear(df.iloc[i, y])
     col = df.columns[y]
-    
-    if val is np.nan or val == 'nan' or col == key_height: 
-      val = ""
+
+    # Append material as list
+    if col == key_patron:
+
+      if builtworks[row_id].get(key_patron) is None:
+        builtworks[row_id][key_patron] = set()
       
-    new_row[col] = val
-  
-  builtworks.get(row_id).append(new_row)
+      builtworks[row_id][key_patron].add(val)
 
-#print(builtworks)
-#write_file('output', json.dumps(builtworks), ext='json')
+    else:
+      builtworks[row_id][col] = val
 
-cnt_id = 1
-bw_id = f'SS_BLDG_{cnt_id:03d}'
-
-while bw_id in builtworks:
-
-  first = builtworks.get(bw_id)[0]
+for bw_id, bw in builtworks.items():
 
   # Create the current row
   xml_row = et.Element(key_row)
 
-  row_date = first[key_date]
+  row_date = bw.get(key_date)
 
   # Shared elements
 
@@ -192,25 +192,25 @@ while bw_id in builtworks:
   # IslandName
   island = et.SubElement(xml_row, key_islandname)
 
-  current_island = get_bw_island(first[key_islandname])
+  current_island = get_bw_island(bw.get(key_islandname))
 
   base_tag(island, key_ita,  current_island.get(key_json_name))
   base_tag(island, key_geo,  current_island.get(key_json_geonames))
 
   # Start_Earliest 
-  base_tag(xml_row, key_start,  first[key_start])
+  base_tag(xml_row, key_start,  bw.get(key_start))
 
   # End_Latest 
-  base_tag(xml_row, key_end,  first[key_end])
+  base_tag(xml_row, key_end,  bw.get(key_end))
 
   # Name 
-  base_tag(xml_row, key_name,  first[key_name])
+  base_tag(xml_row, key_name,  bw.get(key_name))
 
   # Height
-  base_tag(xml_row, key_height,  first[key_height])
+  base_tag(xml_row, key_height,  bw.get(key_height))
 
   # Material
-  text_materials = first[key_material] 
+  text_materials = bw.get(key_material)
 
   if text_materials is not None:
     text_materials = text_materials.split(';')
@@ -222,21 +222,26 @@ while bw_id in builtworks:
 
       current_material = get_bw_material(text_material)
 
-      base_tag(material, key_eng, current_material.get(key_json_name))
-      base_tag(material, key_ita, current_material.get(key_json_ita))
-      base_tag(material, key_aat, current_material.get(key_json_aat))
+      try:
+        base_tag(material, key_eng, current_material.get(key_json_name))
+      except AttributeError as err:
+        pass
+      
+      try:
+        base_tag(material, key_ita, current_material.get(key_json_ita))
+      except AttributeError as err:
+        pass
 
-  # Architect
-  base_tag(xml_row, key_architect,  first[key_architect])
-
-  # Patron
-  base_tag(xml_row, key_patron,  first[key_patron])
+      try:
+        base_tag(material, key_aat, current_material.get(key_json_aat))
+      except AttributeError as err:
+        pass
 
   # SHP_Lenght
-  # base_tag(xml_row, key_shape_lenght,  first[key_shape_lenght])
+  # base_tag(xml_row, key_shape_lenght,  bw[key_shape_lenght])
 
   # SHP_Area
-  # base_tag(xml_row, key_shape_area,  first[key_shape_area])
+  # base_tag(xml_row, key_shape_area,  bw[key_shape_area])
 
   set_uses = dict()
   set_patrons = dict()
@@ -246,36 +251,29 @@ while bw_id in builtworks:
   set_tenants = dict()
   set_functions = dict()
 
-  # Not shared elements
-  for current_bw in builtworks[bw_id]:
+  # Function
+  if bw.get(key_function) not in set_functions:
+    set_functions[bw.get(key_function)] = {key_function_start: bw.get(key_function_start), key_function_end: bw.get(key_function_end)}
 
-    # Function
-    if current_bw[key_function] not in set_functions:
-      set_functions[current_bw[key_function]] = {key_function_start: current_bw[key_function_start], key_function_end: current_bw[key_function_end]}
+  # Use
+  if bw.get(key_use) not in set_uses and bw.get(key_use):
+    set_uses[bw.get(key_use)] = {key_use_start: bw.get(key_use_start), key_use_end: bw.get(key_use_end)}
 
-    # Use
-    if current_bw[key_use] not in set_uses and current_bw[key_use]:
-      set_uses[current_bw[key_use]] = {key_use_start: current_bw[key_use_start], key_use_end: current_bw[key_use_end]}
+  # Typologies
+  if bw.get(key_typology) not in set_typologies and bw.get(key_typology):
+    set_typologies[bw.get(key_typology)] = {key_typology_start: bw.get(key_typology_start), key_typology_end: bw.get(key_typology_end)}
 
-    # Typologies
-    if current_bw[key_typology] not in set_typologies and current_bw[key_typology]:
-      set_typologies[current_bw[key_typology]] = {key_typology_start: current_bw[key_typology_start], key_typology_end: current_bw[key_typology_end]}
+  # Owners
+  if bw.get(key_owner) not in set_owners and bw.get(key_owner):
+    set_owners[bw.get(key_owner)] = {key_owner_start: bw.get(key_owner_start), key_owner_end: bw.get(key_owner_end)}
 
-    # Owners
-    if current_bw[key_owner] not in set_owners and current_bw[key_owner]:
-      set_owners[current_bw[key_owner]] = {key_owner_start: current_bw[key_owner_start], key_owner_end: current_bw[key_owner_end]}
+  # Tenants
+  if type(bw.get(key_tenant)) is str and bw.get(key_tenant) not in set_tenants:
+    set_tenants[bw.get(key_tenant)] = {key_tenant_start: bw.get(key_tenant_start), key_tenant_end: bw.get(key_tenant_end)}
 
-    # Tenants
-    if current_bw[key_tenant] not in set_tenants and current_bw[key_tenant]:
-      set_tenants[current_bw[key_tenant]] = {key_tenant_start: current_bw[key_tenant_start], key_tenant_end: current_bw[key_tenant_end]}
-
-    # Architect
-    if current_bw[key_architect] not in set_architects and current_bw[key_architect]:
-      set_architects[current_bw[key_architect]] = current_bw[key_architect]
-
-    # Patrons
-    if current_bw[key_patron] not in set_patrons and current_bw[key_patron]:
-      set_patrons[current_bw[key_patron]] = current_bw[key_patron]
+  # Architect
+  if type(bw.get(key_architect)) is str and bw.get(key_architect) not in set_architects:
+    set_architects[bw.get(key_architect)] = bw.get(key_architect)
 
   # Function
   functions = et.SubElement(xml_row, key_functions)
@@ -312,7 +310,7 @@ while bw_id in builtworks:
 
     base_tag(typology_tag, key_eng, current_typology.get(key_json_name))
     base_tag(typology_tag, key_ita, current_typology.get(key_json_ita))
-    base_tag(typology_tag, key_aat, str(int(current_typology.get(key_json_aat))))
+    #base_tag(typology_tag, key_aat, str(int(current_typology.get(key_json_aat))))
 
     base_tag(typology_tag, key_typology_start, typology[key_typology_start])
     base_tag(typology_tag, key_typology_end, typology[key_typology_end])
@@ -351,7 +349,7 @@ while bw_id in builtworks:
 
   # Patrons
   patrons = et.SubElement(xml_row, key_patrons)
-  for key, patron in set_patrons.items():
+  for key in bw.get(key_patron):
     patron = et.SubElement(patrons, key_patron)
 
     add_person(patron, key)
@@ -360,8 +358,3 @@ while bw_id in builtworks:
   final = md.parseString(et.tostring(xml_row, method='xml')).toprettyxml()
 
   write_file(bw_id, final)
-
-  break
-
-  cnt_id += 1
-  bw_id = f'SS_BLDG_{cnt_id:03d}'
