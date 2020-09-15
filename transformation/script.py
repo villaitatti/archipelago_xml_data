5dba7af57443de31e06fdba93b57d7a844f205ff
gspinaci <gianmarco.spinaci@gmail.com>
2020-09-15 17:58:17 +0200

    table as cli param

--- transformation/script.py
+++ transformation/script.py
@@ -1,13 +1,13 @@
 import os
+import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def base_path(name):
     return os.path.join(dir_path, name)
 
-#TODO: table should be inserted as cli parameter
-tables = ['builtwork', 'event', 'people', 'source', 'bibitem']
-table_folder = base_path(tables[0])
+# Table is a CLI param
+table_folder = base_path(sys.argv[-1])
 
 table_folder_in = os.path.join(table_folder, 'data')
 table_folder_out = os.path.join(table_folder, 'out')
@@ -23,6 +23,8 @@ for root, dirs, src_files in os.walk(table_folder_in):
     total = len(src_files)
     cnt = 1
 
+    src_files = sorted(src_files)
+
     for src_file in src_files:
 
         src_file_full = os.path.join(table_folder_in, src_file)
@@ -36,9 +38,10 @@ for root, dirs, src_files in os.walk(table_folder_in):
 
             out_file_full = os.path.join(table_folder_out, out_file)
 
-            print(f'Running => {cnt}/{total}')
+            print(f'\n{out_file}\n\nRunning => {cnt}/{total}')
             os.system(f'java -jar {engine} -i {src_file_full} -x {mapping_x3ml} -p {policy} -o {out_file_full} -f {ext}')
 
             cnt += 1
+            break
 
 'java -jar x3ml-engine-1.9.0-exejar.jar -i data/166.xml -x archipelago.x3ml -policy ../../archipelago_generator_policy.xml -o output.ttl -f text/turtle'
