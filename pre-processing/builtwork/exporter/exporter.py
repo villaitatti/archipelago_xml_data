96aec94659ef06aa973664fba2efdd693597af58
gspinaci <gianmarco.spinaci@gmail.com>
2021-01-05 23:43:11 +0100

    Python method to export all tables

---
+++ pre-processing/builtwork/exporter/exporter.py
@@ -0,0 +1,31 @@
+import psycopg2
+import psycopg2.extras
+from dotenv import load_dotenv
+import sys
+import os
+load_dotenv()
+
+q='query.sql'
+
+#connect to db via .env
+conn = psycopg2.connect(host=os.getenv('POSTGRES_HOST'), 
+    dbname=os.getenv('POSTGRES_DATABASE'),
+    user=os.getenv('POSTGRES_USERNAME'),
+    password=os.getenv('POSTGRES_PASSWORD'))
+
+#get all tables
+dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+dict_cur.execute(open(q).read())
+
+data = dict_cur.fetchall()
+
+f=open('data.tsv', 'w')
+#Removed height and material because sansecondo_island_1500 doesn't have it. haven't controller others
+#BW_ID	IslandName	Date	Start	End	Name	Function	Start_Function	End_Function	Use	Start_Use	End_Use	Typology	Start_Typology	End_Typology	Material	Architect	Patron	Owner	Start_Owner	End_Owner	Tenant	Start_Tenant	End_Tenant	SHP_Lenght	SHP_Area
+cols = ['"BW_ID"','"IslandName"','"Date"','"Start"','"End"','"Name"','"Function"','"Start_Function"','"End_Function"','"Use"','"Start_Use"','"End_Use"','"Typology"','"Start_Typology"','"End_Typology"','"Material"','"Architect"','"Patron"','"Owner"','"Start_Owner"','"End_Owner"','"Tenant"','"Start_Tenant"','"End_Tenant"','"SHP_Lenght"','"SHP_Area"']
+
+for tab in data:
+    dict_cur.copy_to(f, tab[0], sep='\t', null='', columns=cols)
+
+dict_cur.close()
+conn.close()
--- pre-processing/builtwork/exporter/query.sql
+++ pre-processing/builtwork/exporter/query.sql
@@ -1,2 +1,2 @@
-\copy (select "BW_ID","IslandName","Date","Start","End","Name","Function","Start_Function","End_Function","Use","Start_Use","End_Use","Typology","Start_Typology","End_Typology","Height","Material","Architect","Patron","Owner","Start_Owner","End_Owner","Tenant","Start_Tenant","End_Tenant","SHP_Lenght","SHP_Area"
- from "public"."sansecondo_buildings_1697") TO 'DATA.tsv' CSV HEADER DELIMITER E'\t'
+select distinct quote_ident(c.table_name) as tabs from information_schema.columns c join information_schema.tables t on t.table_name=c.table_name and t.table_schema=c.table_schema where t.table_name not like 'spatial_%' and t.table_name like 'sansecondo_buildings%' or t.table_name like 'sansecondo_open_spaces%' and t.table_type='BASE TABLE' and t.table_schema = 'public' ;
+
