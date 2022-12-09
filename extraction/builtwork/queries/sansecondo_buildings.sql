f9ea6497f52950230ebd7597f7e84b1d3a2fe4db
gspinaci <gianmarco.spinaci@gmail.com>
2022-12-09 15:57:54 +0100

    New version with cartographic years
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- extraction/builtwork/queries/sansecondo_buildings.sql
+++ extraction/builtwork/queries/sansecondo_buildings.sql
@@ -33,19 +33,46 @@ select id, geometry, "BW_ID", "IslandName", "Date", "Start", "End", "Name", "Fun
 from sansecondo_buildings_2019;
 
 -- Create sansecondo_buildings
-drop table if exists __sansecondo_buildings;
-create table __sansecondo_buildings as 
-select distinct "BW_ID" as identifier, date(null) as "start_bob", date("Start") as "start_eob", 
-case when "End" = '' then date(null) else date("End") end as "end_boe", date(null) as "end_eoe", geometry,
-'Buildings' as t, 2 as z
+drop table if exists __sansecondo_buildings_tmp;
+create table __sansecondo_buildings_tmp as 
+select distinct 
+  "BW_ID" as identifier, 
+  case when "Date" = '1500' then 1 else 0 end as "1500",
+  case when "Date" = '1697' then 1 else 0 end as "1697",
+  case when "Date" = '1717' then 1 else 0 end as "1717",
+  case when "Date" = '1789' then 1 else 0 end as "1789",
+  case when "Date" = '1839' then 1 else 0 end as "1839",
+  case when "Date" = '1850' then 1 else 0 end as "1850",
+  case when "Date" = '1852' then 1 else 0 end as "1852",
+  case when "Date" = '1945' then 1 else 0 end as "1945",
+  case when "Date" = '1982' then 1 else 0 end as "1982",
+  case when "Date" = '2019' then 1 else 0 end as "2019",
+  geometry,
+  'Buildings' as t, 
+  2 as z
 from sansecondo_buildings_data;
 
--- Fix errors: see readme.md
-delete from __sansecondo_buildings
-where identifier = 'SS_BLDG_052' and end_boe = '1788-12-31';
+drop table if exists __sansecondo_buildings;
+create table __sansecondo_buildings as
+select distinct 
+  identifier,
+  CAST(CASE WHEN SUM(CAST("1500" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1500",
+  CAST(CASE WHEN SUM(CAST("1697" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1697",
+  CAST(CASE WHEN SUM(CAST("1717" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1717",
+  CAST(CASE WHEN SUM(CAST("1789" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1789",
+  CAST(CASE WHEN SUM(CAST("1839" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1839",
+  CAST(CASE WHEN SUM(CAST("1850" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1850",
+  CAST(CASE WHEN SUM(CAST("1852" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1852",
+  CAST(CASE WHEN SUM(CAST("1945" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1945",
+  CAST(CASE WHEN SUM(CAST("1982" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "1982",
+  CAST(CASE WHEN SUM(CAST("2019" AS INT)) > 0 THEN 1 ELSE 0 END AS bool) AS "2019",
+  'Buildings' as t, 2 as z, geometry
+from __sansecondo_buildings_tmp
+group by identifier, t, z, geometry;
 
-delete from __sansecondo_buildings
-where identifier = 'SS_BLDG_001' and end_boe = '2019-12-31';
+drop table if exists __sansecondo_buildings_tmp;
+
+-- Fix errors: see readme.md
 
 -- Set the BW_ID as primary key
 ALTER TABLE __sansecondo_buildings
