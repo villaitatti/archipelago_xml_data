06ff5f8d8e35bdfab2302723cfd50e85d82f1a17
gspinaci <gianmarco.spinaci@gmail.com>
2023-01-17 17:13:41 +0100

    Update Source pipeline
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- preprocessing/source/script.py
+++ preprocessing/source/script.py
@@ -1,3 +1,4 @@
+import uuid
 from xml.dom import minidom as md, getDOMImplementation
 import xml.etree.ElementTree as et
 import os
@@ -7,34 +8,82 @@ import dateutil.parser
 import pandas as pd
 from datetime import datetime
 
+
 def execute(limit):
 
+  # Store paths
   dir_path = os.path.dirname(os.path.realpath(__file__))
-  filename = os.path.join(dir_path, os.pardir, os.pardir, 'extraction/transformed/Sources.xml')
-  
-  # Geonames dictionary
-  #geonames_dict = json.load(open(os.path.join(dir_path, '../geonames/', 'geonames.json'), 'r'))
-  association_tables = json.load(open(os.path.join(dir_path, os.pardir, os.pardir, 'utils', 'association_tables', 'association_tables.json'), 'r'))
+  filename = os.path.join(dir_path, os.pardir, os.pardir,
+                          'extraction', 'transformed', 'Sources.xml')
+  output_directory = os.path.join(
+      dir_path, os.pardir, os.pardir, 'transformation', 'source', 'data')
 
+  if not os.path.isdir(output_directory):
+    os.mkdir(output_directory)
 
+  # File with all UUIDs
+  uuid_filename = os.path.join(dir_path, 'source.json')
+  actor_filename = os.path.join(dir_path, 'actor.json')
+
+  # Store association tables
+  association_tables = json.load(open(os.path.join(
+      dir_path, os.pardir, os.pardir, 'utils', 'association_tables', 'association_tables.json'), 'r'))
+
+  # Method called to write data file
   def write_file(text):
-      output_directory = os.path.join(dir_path, os.pardir, os.pardir, 'transformation/source/' ,'data')
-      if not os.path.isdir(output_directory):
-          os.mkdir(output_directory)
-      output_filename = os.path.join(output_directory, f'{row_id}.xml')
-      with open(output_filename, 'w') as f:
-          f.write(text)
+    output_filename = os.path.join(output_directory, f'{row_id}.xml')
+    with open(output_filename, 'w') as f:
+      f.write(text)
 
+  # Method to create a base tag with only text
+  def base_tag(parent, key, text):
+    # Block None text
+    if text is None:
+      return
 
-  def add_clean_field(input_key, tag=None):
-    if tag is None:
-      field = et.SubElement(new_row, input_key)
-    else:
-      field = et.SubElement(new_row, tag)
+    # Block empty and wrong end
+    text = str(text)
+    if not text:
+      return
 
-    field.text = row.find(f'ns:{input_key}', ns).text
+    tag = et.SubElement(parent, key)
+    tag.text = text
 
-  KEY_PARENT = 'parents'
+  # Method to retrieve value from a key file
+  def get_value_from_key(key):
+    return row.find(f'ns:{key}', ns).text
+
+  # Method that transform typology into its ID
+  # by lowering all letters and replacing whitespaces with _
+  def get_id_from_typology(typology):
+    typology = str(typology).replace(' ', '_')
+    return typology.lower()
+
+  # List of KEYS
+
+  KEY_UUID_SOURCE = 'uuid_source'
+  KEY_OUT_ROW = 'source'
+  KEY_OUT_LABEL = 'label'
+
+  KEY_IN_ID = 'ID_SOURCE'
+  KEY_IN_TITLE = 'Title'
+  KEY_IN_ORIGINALTITLE = 'Original_Title'
+  KEY_IN_SYNOPSIS = 'Synopsis'
+  KEY_IN_TRANSCRIPTION = 'Trascription'
+  KEY_IN_LANGUAGE = 'Language'
+  KEY_IN_TYPOLOGY = 'Typology'
+  KEY_IN_MEDIUM = 'Medium'
+  KEY_IN_DIMENSIONS = 'Dimensions'
+
+  KEY_OUT_ID = KEY_IN_ID.lower()
+  KEY_OUT_TITLE = KEY_IN_TITLE.lower()
+  KEY_OUT_ORIGINALTITLE = KEY_IN_ORIGINALTITLE.lower()
+  KEY_OUT_SYNOPSIS = KEY_IN_SYNOPSIS.lower()
+  KEY_OUT_TRANSCRIPTION = KEY_IN_TRANSCRIPTION.lower()
+  KEY_OUT_LANGUAGE = KEY_IN_LANGUAGE.lower()
+  KEY_OUT_TYPOLOGY = KEY_IN_TYPOLOGY.lower()
+  KEY_OUT_MEDIUM = KEY_IN_MEDIUM.lower()
+  KEY_OUT_DIMENSIONS = KEY_IN_DIMENSIONS.lower()
 
   keys = {
       "row": "ROW",
@@ -71,224 +120,89 @@ def execute(limit):
       "day_latest": "Day_Latest"
   }
 
-  custom_keys = {
-      "date_earliest": "Date_Earliest",
-      "date_latest": "Date_Latest",
-  }
-
-  exceptions = {
-      "Lagoon": "Venetian Lagoon",
-      "San Giorgio Maggiore": "Isola di San Giorgio Maggiore"
-  }
-
+  # Read file
   tree = et.parse(filename)
   root = tree.getroot()
   ns = {'ns': 'http://www.filemaker.com/fmpdsoresult'}
 
+  # UUID dictionaries
+  uuid_dict = {}
+  if os.path.isfile(uuid_filename):
+    uuid_dict = json.load(open(uuid_filename))
+
+  actor_dict = {}
+  if os.path.isfile(actor_filename):
+    actor_dict = json.load(open(actor_filename))
+
+  ##############################################################################################################
+  ####################################### PRE-PROCESSING #######################################################
+  ##############################################################################################################
+  cnt = 0
   tags = root.findall(f'ns:{keys["row"]}', ns)
 
-  doc = md.Document()
-
-  #new_document_root = et.Element('FMPDSORESULT', ns)
-
-  archival_units = set()
-
-  def add_archival_unit(unit, type, parent):
-    if unit is not None:
-      archival_units.add((unit, parent, type))
-    
-
-  cnt = 0
-
-  # Iterate each ROW
   for row in tags:
 
+    # If the limit is reached.
+    # Limit is passed as an arg when running the script
     if limit and cnt == int(limit):
       break
 
-    # Copy the current row
-    new_row = et.Element(keys["row"])
+    # Save id_source, which is also the KEY to store the UUID
+    id_source = get_value_from_key(KEY_IN_ID)
 
-    # ROW ID
-    row_id = row.find(f'ns:{keys["id_source"]}', ns).text
-    id_source = et.SubElement(new_row, keys["id_source"])
-    id_source.text = row_id
+    # Add this source to the UUID dict if not already there
+    if id_source not in uuid_dict:
+      uuid_dict[id_source] = {
+          KEY_UUID_SOURCE: str(uuid.uuid1())
+      }
 
-    # Add clean fields
-    add_clean_field(keys["title"], 'attributed_title')
-    #add_clean_field(keys["island"])
-    #add_clean_field(keys["typology"])
-    add_clean_field(keys["format"], 'typology')
+    # root tag
+    new_row = et.Element(KEY_OUT_ROW)
 
-    #add_clean_field(keys["author_surname_name"])
-    add_clean_field(keys["role_author"])
+    # Get and store UUID
+    row_id = uuid_dict[id_source][KEY_UUID_SOURCE]
+    base_tag(new_row, KEY_OUT_ID, row_id)
 
-    add_clean_field(keys["original_title"])
-    add_clean_field(keys["language"])
+    # Attributed title
+    attributed_title = get_value_from_key(KEY_IN_TITLE)
+    base_tag(new_row, KEY_OUT_TITLE, attributed_title)
 
-    #add_clean_field(keys["century"])
-    #add_clean_field(keys["fraction_century"])
-    #add_clean_field(keys["collection"])
-    #add_clean_field(keys["acronym"])
-    #add_clean_field(keys["fondo"])
-    #add_clean_field(keys["busta"])
-    #add_clean_field(keys["filza"])
-    #add_clean_field(keys["title_filza"])
-    #add_clean_field(keys["folio"])
-    #add_clean_field(keys["drawing"])
-    #add_clean_field(keys["location"])
-    #add_clean_field(keys["medium"])
-    #add_clean_field(keys["dimensions"])
-    add_clean_field(keys["synopsis"])
-    add_clean_field(keys["trascription"])
+    # Original title
+    original_title = get_value_from_key(KEY_IN_ORIGINALTITLE)
+    base_tag(new_row, KEY_OUT_ORIGINALTITLE, original_title)
 
-    # Add Custom Fields
-    # Date_Earliest
-    day_earliest = row.find(f'ns:{keys["day_earliest"]}', ns).text
-    if day_earliest is None:
-        day_earliest = "01"
-    month_earliest = row.find(f'ns:{keys["month_earliest"]}', ns).text
-    if month_earliest is None:
-        month_earliest = "01"
-    year_earliest = row.find(f'ns:{keys["year_earliest"]}', ns).text
-    date_earliest_pretty = f'{year_earliest}-{month_earliest}-{day_earliest}T00:00:00'
-    if year_earliest is None:
-        date_earliest_pretty = ""
-    # Date format: YYYY-MM-DDThh:mm:ss (xsd:datetime)
-    date_earliest = et.SubElement(new_row, custom_keys["date_earliest"])
-    date_earliest.text = date_earliest_pretty
+    # Synopsis
+    synopsis = get_value_from_key(KEY_IN_SYNOPSIS)
+    base_tag(new_row, KEY_OUT_SYNOPSIS, synopsis)
 
-    # Date_Latest
-    day_latest = row.find(f'ns:{keys["day_latest"]}', ns).text
-    if day_latest is None:
-        day_latest = "01"
-    month_latest = row.find(f'ns:{keys["month_latest"]}', ns).text
-    if month_latest is None:
-        month_latest = "01"
-    year_latest = row.find(f'ns:{keys["year_latest"]}', ns).text
-    date_latest_pretty = f'{year_latest}-{month_latest}-{day_latest}T00:00:00'
-    if year_latest is None:
-        date_latest_pretty = ""
-    # Date format: YYYY-MM-DDThh:mm:ss (xsd:datetime)
-    date_latest = et.SubElement(new_row, custom_keys["date_latest"])
-    date_latest.text = date_latest_pretty
+    # Transcription and Language
+    tag_transcription = et.SubElement(new_row, KEY_OUT_TRANSCRIPTION)
 
-    archival_path = ''
+    transcription = get_value_from_key(KEY_IN_TRANSCRIPTION)
+    base_tag(tag_transcription, KEY_OUT_LABEL, transcription)
 
-    # Archives
-    archive = row.find(f'ns:{keys["collection"]}', ns).text
-    if archive is not None:
-      archival_path += archive
+    language = get_value_from_key(KEY_IN_LANGUAGE)
+    base_tag(tag_transcription, KEY_OUT_LANGUAGE, language)
+
+    # Typology
+    typology = get_id_from_typology(get_value_from_key(KEY_IN_TYPOLOGY))
+    base_tag(new_row, KEY_OUT_TYPOLOGY, typology)
     
-    # Fonds
-    fond = row.find(f'ns:{keys["fondo"]}', ns).text 
-    if fond is not None:
-      archival_path += f' > {fond}'
-
-    # Folder
-    folder = row.find(f'ns:{keys["busta"]}', ns).text
-    if folder is not None:
-      archival_path += f' > {folder}'
-
-    # Filza
-    file = row.find(f'ns:{keys["filza"]}', ns).text
-    if file is not None:
-      archival_path += f' > {file}'
+    # Medium
+    medium = get_value_from_key(KEY_IN_MEDIUM)
+    base_tag(new_row, KEY_OUT_MEDIUM, medium)
     
-    # Folio
-    folio = row.find(f'ns:{keys["folio"]}', ns).text
-    if folio is not None:
-      archival_path += f' > {folio}'
+    # Dimension
+    dimensions = get_value_from_key(KEY_IN_DIMENSIONS)
+    base_tag(new_row, KEY_OUT_DIMENSIONS, dimensions)
 
-    # Drawing
-    drawing = row.find(f'ns:{keys["drawing"]}', ns).text
-    if drawing is not None:
-      archival_path += f' > {drawing}'
+    # TODO parse actor name to retrieve its UUID
 
-    name = row.find(f'ns:{keys["title"]}', ns).text
-    archival_units.add((row_id, archival_path, name))
-
-
-    
-
-    """
-    # Archives
-    archive = row.find(f'ns:{keys["collection"]}', ns).text
-    parent = None
-    add_archival_unit(archive, 'https://archipelago.itatti.harvard.edu/resource/vocab/archive', parent)
-
-    # Fonds
-    fond = row.find(f'ns:{keys["fondo"]}', ns).text
-    parent = archive
-    add_archival_unit(fond, 'https://archipelago.itatti.harvard.edu/resource/vocab/fond', parent)
-
-    # Folder
-    folder = row.find(f'ns:{keys["busta"]}', ns).text
-    if fond is not None:
-      parent = fond
-    add_archival_unit(folder, 'https://archipelago.itatti.harvard.edu/resource/vocab/folder', parent)
-
-    # Filza
-    file = row.find(f'ns:{keys["filza"]}', ns).text
-    if folder is not None:
-      parent = folder
-    add_archival_unit(file, 'https://archipelago.itatti.harvard.edu/resource/vocab/archival_unit', parent)
-
-    # Folio and Drawing
-    if file is not None:
-      parent = file 
-
-    folio = row.find(f'ns:{keys["folio"]}', ns).text
-    add_archival_unit(folio, 'https://archipelago.itatti.harvard.edu/resource/vocab/folio', parent)
-
-    drawing = row.find(f'ns:{keys["drawing"]}', ns).text
-    add_archival_unit(drawing, 'https://archipelago.itatti.harvard.edu/resource/vocab/drawing', parent)
-    """
-
-    """
-    # Island
-    island_found = row.find(f'ns:{keys["island"]}', ns).text
-    if island_found is None:
-            isl_geoname_id = ""
-    else:
-        if island_found not in geonames_dict:
-            island_found = exceptions[island_found]
-        isl_geoname_id = geonames_dict[island_found][0]["geoname_id"]
-    island = et.SubElement(new_row, keys["island"])
-    island.text = isl_geoname_id
-    """
-
-    #ASSOCIATIONS
-
-    #Events
-    events = et.SubElement(new_row, "EVENTS")
-    if row_id in association_tables['sources']:
-        for k,v in association_tables['sources'][row_id]['events'].items():
-            event = et.SubElement(events, "EVENT")
-            event_id = et.SubElement(event, "ID_EVENT")
-            event_id.text = k
-            event_description = et.SubElement(event, "Event")
-            event_description.text = v["Event"]
-
-    #People
-    people = et.SubElement(new_row, "PEOPLE")
-    if row_id in association_tables['sources']:
-        for k,v in association_tables['sources'][row_id]['people'].items():
-            person = et.SubElement(people, "PERSON")
-            person_id = et.SubElement(person, "ID_PERSON")
-            person_id.text = k
-            name = et.SubElement(person, "Name")
-            name.text = v["Name"]
-            surname = et.SubElement(person, "Surname")
-            surname.text = v["Surname"]
-
-    #new_document_root.append(new_row)
     final = md.parseString(et.tostring(new_row, method='xml')).toprettyxml()
     write_file(final)
 
     cnt += 1
 
-  df = pd.DataFrame(list(archival_units), columns=['id', 'path', 'name'])
-  df = df.set_index('id')
-  df = df.sort_values('path')
-  df.to_csv(os.path.join(dir_path, 'archival_units.csv'), quoting=csv.QUOTE_NONNUMERIC)
+  # Save file with UUIDs
+  with open(uuid_filename, 'w') as f:
+    f.write(json.dumps(uuid_dict, indent=4, sort_keys=True))
